1dbdaefdad3a791733b483e565b4174264547085
Garn1k <garnik200318@gmail.com>
2025-01-09 17:42:11 +0400

    new commit

--- server.js
+++ server.js
@@ -1,19 +1,58 @@
 const http = require("http");
-const express = require("express");
 const path = require("path");
-const cors = require("cors");
-const {Server} = require("socket.io");
+const express = require("express");
+const { Server } = require("socket.io");
+const cors = require("cors")
+
 require("dotenv").config();
+
+// const router = require("./src/routers/router")
+ 
+
+const PORT = process.env.PORT || 8000;
+
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-const PORT = process.env.PORT;
-app.use(cors());
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+const server = http.createServer(app);  
+const io = new Server(server); 
+
+console.log(path.join(__dirname, "/public"));
 
 
+app.use(express.static(path.join(__dirname, "/public")));
+
+
+app.use(cors())
+app.use(express.json())
+
+// app.use(router)
+let socketsConnected = new Set();
+
+
+
+io.on("connection", (socket) => {
+    console.log("Socket connected", socket.id);
+    socketsConnected.add(socket.id);
+
+    io.emit('clients-total', socketsConnected.size);
+
+    socket.on("message", (data) => {
+        console.log("data",data);
+        io.emit("chat-message", data)
+    })
+   
+     
+
+    socket.on("feedback", (data) => {
+        socket.broadcast.emit("feedback", data);
+    });
+
+    socket.on("disconnect",()=>{
+        console.log("Socket disconnected", socket.id);
+        socketsConnected.delete(socket.id);
+        io.emit("clients-total", socketsConnected.size)
+    })
+})
 
 server.listen(PORT, () => {
-    console.log("Server is running on port 5000");
-})
+  console.log(`SERVER LISTEN ${PORT}`);
+});
